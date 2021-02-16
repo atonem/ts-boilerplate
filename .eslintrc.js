30ae1f936e68f9e5f24c4d4d07e6346672bf8b0c
Anton Emmerik <anton@emmerik.se>
2021-02-16 12:30:30 +0700

    setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,16 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'airbnb-typescript/base',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
+  plugins: ['@typescript-eslint', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
